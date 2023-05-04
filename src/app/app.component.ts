4fe543c4ef55596562526a2d5fdf044aaeec635a
hiya <34233282+hiyabh@users.noreply.github.com>
2023-05-04 23:54:54 +0300

    Calculator_Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,95 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-}
+
+    screen1: number = 0;
+    screen2: number = 0;
+    operation: string = "";
+    memorys: any = [];
+    class_mem {};
+          
+};
+
+
+    title = 'app';
+
+    //פונקציית לחיצה על ספרות
+    click_number(number) {
+        this.screen1 = this.screen1 * 10 + number;
+    };
+
+    //פונקציית איפוס צג תוצאה
+    clear() {
+        this.screen1 = 0;
+        this.screen2 = 0;
+        this.operation = '';
+    };
+
+    //פונקציית +
+    click_plus() {
+        if (this.operation != '-') {
+            if (this.screen2 == 0) {
+                this.screen2 = this.screen1;
+                this.screen1 = 0;
+                this.operation = '+';
+            }
+            else {
+                this.operation = '+';
+                this.screen2 = this.screen2 + this.screen1;
+                this.memorys.this.first.push(this.screen1);
+                this.memorys.this.op.push(this.operation);
+                this.memorys.this.secand.push(this.screen2);
+                alert(this.memorys[this.memorys.length-1]);
+                this.screen1 = 0;
+            }
+           
+        };
+        if (this.operation == '-') {
+            this.screen2 = (this.screen2 - this.screen1);
+            this.operation = '+';
+            this.screen1 = 0;
+        };
+        };
+
+    //פונקציית -
+        click_minus() {
+            if (this.operation != '+'){
+                if (this.screen2 == 0) {
+                    this.screen2 = this.screen1;
+                    this.screen1 = 0;
+                    this.operation = '-';
+                };
+                if (this.screen2 != 0) {
+                  this.operation = '-';
+                  this.screen2 = (this.screen2 - this.screen1);
+                  this.screen1 = 0;
+                };
+             
+            };
+            if (this.operation == '+') {
+                this.screen2 = (this.screen2 + this.screen1);
+                this.operation = '-';
+                this.screen1 = 0;
+            };        
+        };
+   
+     //פונקציית =
+     click_eval() {
+            if (this.screen2 != 0) {
+                if (this.operation == '+') {
+                    this.screen2 = (this.screen2 + this.screen1);
+                    this.screen1 = 0;
+                };
+                if (this.operation == '-') {
+                    this.screen2 = (this.screen2 - this.screen1);
+                    this.screen1 = 0;
+                };
+            };
+    };
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { MemoryComponent } from './memory/memory.component';
+import { ScreenComponent } from './screen/screen.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemoryComponent,
+    ScreenComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
